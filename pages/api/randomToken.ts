292b238ab787405c80d2217735824b0a8d75042a
Test <deimudda@gmail.com>
2024-09-22 00:05:31 +0200

    api q fix

--- pages/api/randomToken.ts
+++ pages/api/randomToken.ts
@@ -2,11 +2,18 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { MongoClient } from 'mongodb';
 
 const uri = process.env.MONGODB_URI;
-const client = new MongoClient(uri);
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+if (!global._mongoClientPromise) {
+  client = new MongoClient(uri);
+  global._mongoClientPromise = client.connect();
+}
+clientPromise = global._mongoClientPromise;
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
-    await client.connect();
+    const client = await clientPromise;
     const database = client.db('temptokens');
     const collection = database.collection('tokens');
     const tokens = await collection.find().toArray();
@@ -20,7 +27,5 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   } catch (error) {
     console.error('Error:', error);
     res.status(500).json({ error: 'Internal Server Error' });
-  } finally {
-    await client.close();
   }
 }
