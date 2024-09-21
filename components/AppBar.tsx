9ab9dd23d7341e96a94be8ed29168cda9b2bd31e
Test <deimudda@gmail.com>
2024-09-21 04:12:51 +0200

    init

--- components/AppBar.tsx
+++ components/AppBar.tsx
@@ -1,14 +1,21 @@
-import { FC } from 'react'
-import styles from '../styles/Home.module.css'
-import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
-import Image from 'next/image'
+import { FC } from 'react';
+import styles from '../styles/AppHeader.module.css';
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import Image from 'next/image';
 
 export const AppBar: FC = () => {
     return (
         <div className={styles.AppHeader}>
-            <Image src="/solanaLogo.png" height={30} width={200} />
-            <span>Wallet-Adapter</span>
-            <WalletMultiButton />
+            <div className={styles.LeftSection}>
+                <Image src="/solanaLogo.png" height={50} width={50} />
+                <span className={styles.Title}>Matcher on Solana</span>
+            </div>
+            <div className={styles.RightSection}>
+                <button className={styles.NavButton}>Matcher</button>
+                <button className={styles.NavButton}>Profile</button>
+                <button className={styles.NavButton}>Docs</button>
+                <WalletMultiButton className={styles.WalletButton} />
+            </div>
         </div>
-    )
-}
+    );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,25 @@
-import { NextPage } from 'next'
-import styles from '../styles/Home.module.css'
-import WalletContextProvider from '../components/WalletContextProvider'
-import { AppBar } from '../components/AppBar'
-// import { BalanceDisplay } from '../components/BalanceDisplay'
-import { PingButton } from '../components/PingButton'
-import Head from 'next/head'
+import { NextPage } from 'next';
+import styles from '../styles/Home.module.css';
+import WalletContextProvider from '../components/WalletContextProvider';
+import { AppBar } from '../components/AppBar';
+import Head from 'next/head';
+import Footer from '../components/Footer'; // Import Footer component
 
 const Home: NextPage = (props) => {
-
   return (
     <div className={styles.App}>
       <Head>
-        <title>Wallet-Adapter</title>
-        <meta
-          name="description"
-          content="Wallet-Adapter"
-        />
-         <link rel="icon" href="/sol.ico" />
+        <title>Matcher</title>
+        <meta name="description" content="Wallet-Adapter" />
+        <link rel="icon" href="/sol.ico" />
       </Head>
       <WalletContextProvider>
         <AppBar />
-        <div className={styles.AppBody}>
-          <PingButton />
-        </div>
-      </WalletContextProvider >
+        <div className={styles.AppBody}></div>
+        <Footer /> {/* Add Footer component */}
+      </WalletContextProvider>
     </div>
   );
-}
+};
 
 export default Home;
